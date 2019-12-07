9a0cb8df4062f38f8a5e2c486ebdbf73c936d536
liu <1807578838@qq.com>
2019-12-07 06:16:27 +0000

    123

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -10,6 +10,22 @@ class WxController extends Controller
     /*微信*/
     public function wechat()
     {
-        echo '微信开发';/*25.36*/
+        $token='2259b56f5898cd6192c50';
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr=$_GET['echostr'];
+        
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+
+        if( $tmpStr == $signature ){
+          echo $echostr;
+        }else{
+           die('not ok');
+        }
     }
 }
