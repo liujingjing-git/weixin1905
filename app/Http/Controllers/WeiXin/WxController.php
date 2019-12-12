d865153667e54fbc584bec9fa628564eda9aa18a
liu <1807578838@qq.com>
2019-12-12 14:36:02 +0800

    完善

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -46,102 +46,81 @@ class WxController extends Controller
         }
     }
 
-    /*接收微信推送事件*/
-    public function receiv(){
-        $log_file = "wx.log";
-        //将接收到的文件记录到日志文件
+/**
+     * 接收微信推送事件
+     */
+    public function receiv()
+    {
+        $log_file = "wx.log";       // public
+        //将接收的数据记录到日志文件
         $xml_str = file_get_contents("php://input");
-        $data = date('Y-m-d H:i:s').$xml_str;
-        file_put_contents($log_file,$data,FILE_APPEND);
-
+        $data = date('Y-m-d H:i:s')  . ">>>>>>\n" . $xml_str . "\n\n";
+        file_put_contents($log_file,$data,FILE_APPEND);     //追加写
         //处理xml数据
         $xml_obj = simplexml_load_string($xml_str);
-
-        //获取事件类型
-        $event = $xml_obj->Event;
-
+        $event = $xml_obj->Event;       // 获取事件类型
         if($event=='subscribe'){
-            //获取用户的openid
-            $openid=$xml_obj->FromUserName;
-            //判断用户是否已经存在
+            $openid = $xml_obj->FromUserName;       //获取用户的openid
+            //判断用户是否已存在
             $u = WxUserModel::where(['openid'=>$openid])->first();
             if($u){
-           
-              $content = date('Y-m-d H:i:s')."欢迎回来";
-              $touser = $xml_obj->FromUserName; //接收消息的用户openid
-              $fromuser = $xml_obj->ToUserName; //开发者公众号的id
-              $time = time();
-              $response_text = '<xml>
-                <ToUserName><![CDATA['.$touser.']]></ToUserName>
-                <FromUserName><![CDATA['.$fromuser.']]></FromUserName>
-                <CreateTime>'.$time.'</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA['.$content.']]></Content>
+                $msg = '欢迎您再次回来';
+                $xml = '<xml>
+                  <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                  <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
+                  <CreateTime>'.time().'</CreateTime>
+                  <MsgType><![CDATA[text]]></MsgType>
+                  <Content><![CDATA['.$msg.']]></Content>
                 </xml>';
-    
-                //回复用户消息
-                echo $response_text;
-
-
+                echo $xml;
             }else{
+                //获取用户信息
+                $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
+                $user_info = file_get_contents($url);       //
+                $u = json_decode($user_info,true);
+                //echo '<pre>';print_r($u);echo '</pre>';die;
+                //入库用户信息
                 $user_data = [
-                  'openid' => $openid,
-                  'sub_time' => $xml_obj->CreateTime,
+                    'openid'    => $openid,
+                    'nickname'  => $u['nickname'],
+                    'sex'       => $u['sex'],
+                    'headimgurl'    => $u['headimgurl'],
+                    'subscribe_time'    => $u['subscribe_time']
                 ];
                 //openid 入库
                 $uid = WxUserModel::insertGetId($user_data);
-                $content = date('Y-m-d H:i:s')."欢迎关注";
-                $touser = $xml_obj->FromUserName; //接收消息的用户openid
-                $fromuser = $xml_obj->ToUserName; //开发者公众号的id
-                $time = time();
-                $response_text = '<xml>
-                  <ToUserName><![CDATA['.$touser.']]></ToUserName>
-                  <FromUserName><![CDATA['.$fromuser.']]></FromUserName>
-                  <CreateTime>'.$time.'</CreateTime>
-                  <MsgType><![CDATA[text]]></MsgType>
-                  <Content><![CDATA['.$content.']]></Content>
+                $msg = "谢谢您的关注";
+                //回复用户关注
+                $xml = '<xml>
+                    <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                    <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
+                    <CreateTime>'.time().'</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA['.$msg.']]></Content>
                   </xml>';
-      
-                  //回复用户消息
-                  echo $response_text;
+                echo $xml;
             }
-
-            //获取用户信息
-            $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
-            $user_info = file_get_contents($url);
-            file_put_contents('wx_user.log',$user_info,FILE_APPEND);
         }
-
-
-       
-
-        $touser = $xml_obj->FromUserName; //接收消息的用户openid
-        $fromuser = $xml_obj->ToUserName; //开发者公众号的id
-        $time = time();
-
-
-
-         // 判断消息类型
+        // 判断消息类型
         $msg_type = $xml_obj->MsgType;
-
+        $touser = $xml_obj->FromUserName;       //接收消息的用户openid
+        $fromuser = $xml_obj->ToUserName;       // 开发者公众号的 ID
+        $time = time();
         if($msg_type=='text'){
-          $content = date('Y-m-d H:i:s') . $xml_obj->Content;
-          $response_text = '<xml>
+            $content = date('Y-m-d H:i:s') . $xml_obj->Content;
+            $response_text = '<xml>
             <ToUserName><![CDATA['.$touser.']]></ToUserName>
             <FromUserName><![CDATA['.$fromuser.']]></FromUserName>
             <CreateTime>'.$time.'</CreateTime>
             <MsgType><![CDATA[text]]></MsgType>
             <Content><![CDATA['.$content.']]></Content>
-            </xml>';
-
-            //回复用户消息
-            echo $response_text;
+          </xml>';
+            echo $response_text;            // 回复用户消息
         }
-      
-      
-      }
-
-    /*获取用户基本信息*/
+    }
+    /**
+     * 获取用户基本信息
+     */
     public function getUserInfo($access_token,$openid)
     {
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
@@ -149,5 +128,4 @@ class WxController extends Controller
         $json_str = file_get_contents($url);
         $log_file = 'wx_user.log';
         file_put_contents($log_file,$json_str,FILE_APPEND);
-    }
-}
+    }}
