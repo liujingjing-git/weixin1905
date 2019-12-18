d366bdf060cd9fedb5976afdd509c065b14aab3c
liu <1807578838@qq.com>
2019-12-18 19:27:20 +0800

    投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -14,6 +14,11 @@ class VoteController extends Controller
         $data = $this->getAccessToken($code); 
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);    
+
+        //处理业务逻辑
+        $redis_key = 'vote';
+        $number = Redis::incr($redis_key);
+        echo '投票成功,当前票数:'.$number;
     }
 
     /*根据code获取access_token*/
@@ -27,7 +32,12 @@ class VoteController extends Controller
     protected function getUserInfo($access_token,$openid){
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data = file_get_contents($url);
-        $user_info = json_decode($json_data,true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+        $data = json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            die('出错了 40001');  //40001 标识获取用户信息失败
+        }
+
+        //返回用户消息
+        return $data;
     }
 }
