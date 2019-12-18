e86338862e0ce6797e6214981f24ee18c17c0853
liu <1807578838@qq.com>
2019-12-19 07:38:12 +0800

    debug

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -14,7 +14,8 @@ class VoteController extends Controller
         //获取access_token   
         $data = $this->getAccessToken($code); 
         //获取用户信息
-        $user_info = $this->getUserInfo($data['access_token'],$data['openid']);    
+        $user_info = $this->getUserInfo($data['access_token'],$data['openid']); 
+           
 
         //处理业务逻辑  
 
@@ -40,7 +41,7 @@ class VoteController extends Controller
     protected function getAccessToken($code){
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data = file_get_contents($url);
-        return json_decode($json_data,true);
+         dd(json_decode($json_data,true));
     }
 
     //获取用户基本信息
