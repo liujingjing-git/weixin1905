d7b91260ac8305e6b281d9d3fd3dbc02501e3d1f
liu <1807578838@qq.com>
2019-12-20 19:14:49 +0800

    商品详情

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\GoodsModel;
 
 class IndexController extends Controller
 {
     /*商品详情页*/
-    public function detail(){
-        return view('goods.detail');
+    public function detail(Request $request){
+
+        $goods_id = $request->input('id');
+        // echo $goods_id;die;
+        $goods = GoodsModel::find($goods_id);
+        // echo '<pre>';print_r($goods);echo '</pre>';
+        $data = [
+            'goods' => $goods
+        ];
+        return view('goods.detail',$data);
     }
 }
